6d4e355f052cb43a389bb083d1df71105054e2b2
Padmini Pinaka <paddu0242@gmail.com>
2020-09-30 11:50:49 +0400

    Add files via upload

---
+++ AppIce/AppIce.h
@@ -0,0 +1,19 @@
+//
+//  AppIce.h
+//  AppIce
+//
+//  Created by Padmini on 9/30/20.
+//  Copyright © 2020 None. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AppIce.
+FOUNDATION_EXPORT double AppIceVersionNumber;
+
+//! Project version string for AppIce.
+FOUNDATION_EXPORT const unsigned char AppIceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppIce/PublicHeader.h>
+
+
---
+++ AppIce/AppIce.swift
@@ -0,0 +1,19 @@
+//
+//  AppIce.swift
+//  AppIce
+//
+//  Created by Padmini on 9/30/20.
+//  Copyright © 2020 None. All rights reserved.
+//
+
+import Foundation
+
+public class AppIce {
+    let hello = "Hello"
+
+    public init() {}
+
+    public func hello(to whom: String) -> String {
+        return "Hello \(whom)"
+    }
+}
